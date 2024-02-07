550e3409b90dd70833f339d238b52f65b768f6dd
unknown <guilizondo@gmail.com>
2024-02-06 19:42:25 -0800

    added database functions, chatgpt connection, and process to create a profile and add to database

--- database/database.py
+++ database/database.py
@@ -1,26 +1,62 @@
 import sqlite3
 
-# connection = sqlite3.connect("profile.db")
+# connection = sqlite3.connect("resume-bot.db")
+
+
+def check_user_exists(connection, user_id, user_name):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM users WHERE user_id={user_id}")
+    rows = cursor.fetchall()
+    if rows:
+        print("Exists")
+        return True
+    else:
+        print("Does not exist")
+        add_user(connection, user_id, user_name)
+        return False
 
 
 # CREATE TABLE
 def create_table(connection):
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER, name TEXT, lastname TEXT,"
-                   "username TEXT, city TEXT, state TEXT"
-                   ", country TEXT, email TEXT, phone TEXT, linkedin TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, name TEXT, lastname TEXT, "
+                   "city TEXT, state TEXT, country TEXT, email TEXT, phone TEXT, linkedin TEXT)")
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS resumes (resume_id INTEGER PRIMARY KEY, user_id INTEGER,title TEXT, 
+    summary TEXT, FOREIGN KEY (user_id) REFERENCES users(user_id))''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS work_experiences (experience_id INTEGER PRIMARY KEY, resume_id 
+    INTEGER, company_name TEXT, position TEXT, start_date TEXT, end_date TEXT, description TEXT, FOREIGN KEY (
+    resume_id) REFERENCES resumes(resume_id))''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS education (education_id INTEGER PRIMARY KEY, resume_id INTEGER, 
+    institution TEXT, degree TEXT, start_date TEXT, end_date TEXT, description TEXT, FOREIGN KEY (resume_id) 
+    REFERENCES resumes(resume_id))''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS skills (skill_id INTEGER PRIMARY KEY, resume_id INTEGER, skill_name 
+    TEXT, proficiency TEXT, FOREIGN KEY (resume_id) REFERENCES resumes(resume_id))''')
+
     connection.commit()
     connection.close()
 
 
 # ADD ELEMENTS TO THE TABLE
-def add_user(connection, user_id, user_name):
+def add_user(connection, user_id, name):
     cursor = connection.cursor()
     # cursor.execute(f"INSERT INTO user VALUES ({user_id}, {user_name}, '', '', '', '', '', "
     #                "'', '', '' )")
-    cursor.execute(f"INSERT INTO user VALUES ({user_id}, '{user_name}', 'Smith', 'jsmith', 'Portland', 'Oregon', 'US', "
-                   "'email@email.com', '248-123-1234', 'https://www.linkedin.com/in/guillermo-lizondo' )")
-    print(f'Added user with id: {user_id}')
+    cursor.execute(f"INSERT INTO users VALUES ({user_id}, '{name}', 'x', 'x', 'x', 'x', "
+                   "'x', 'x', 'x' )")
+    print(f'Added user with user_id: {user_id}')
+    connection.commit()
+    # connection.close()
+
+
+async def insert_data_user(connection, last_name, city, state, country, email, phone, linkedin, user_id):
+    cursor = connection.cursor()
+    sql_update_query = f"""UPDATE users SET lastname = ?, city = ?, state = ?, country = ?, email = ?, phone = ?, 
+    linkedin = ? WHERE user_id = {user_id}"""
+    cursor.execute(sql_update_query, (last_name, city, state, country, email, phone, linkedin))
     connection.commit()
     connection.close()
 
@@ -28,7 +64,7 @@ def add_user(connection, user_id, user_name):
 # GET ELEMENTS FROM TABLE
 def get_all_users(connection):
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM user")
+    cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
@@ -39,7 +75,8 @@ def get_all_users(connection):
 # DELETE ELEMENTS
 def delete_user(connection, user_id):
     cursor = connection.cursor()
-    cursor.execute("DELETE FROM user WHERE id=" + user_id)
+    print(f'Deleting {user_id}')
+    cursor.execute(f"DELETE FROM users WHERE id={user_id}")
     connection.commit()
     connection.close()
 
@@ -53,3 +90,6 @@ def delete_user(connection, user_id):
 
 # add_user(1, 'Luis')
 # get_all_users(connection)
+# delete_user(connection, 1531227019)
+
+# create_table(connection)
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 # pip install -r requirements.txt
 
 import logging
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackContext, \
-    CallbackQueryHandler
 from pathlib import Path
-import numpy as np
-import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
 from database import database
 from credentials import credentials
 import sqlite3
-
+from docx import Document
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -18,18 +15,110 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-connection = sqlite3.connect("./database/profile.db")
+connection = sqlite3.connect("./database/resume-bot.db")
+
+LASTNAME, CITY, STATE, COUNTRY, EMAIL, PHONE, LINKEDIN, RESUME = range(8)
 
 
 # COMMANDS
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    print(f'{user}')
-    user_id = user.id
-    user_name = user.first_name
-    database.add_user(connection, user_id, user_name)
+    user_id = update.effective_user.id
+    user_name = update.effective_user.first_name
+    print(f'{user_id} {user_name}')
+    # Check if user exists, add if not add new one
+    exists = database.check_user_exists(connection, user_id, user_name)
     print("Start command ran")
-    await update.message.reply_html(rf"Welcome to the Resume Tailoring Tool. go to /help to find out what you can do")
+    if not exists:
+        await update.message.reply_html(
+            rf"Welcome to the Resume Tailoring Tool. It looks like you are new here, {user_name}. Please access your /profile to add information that will be added to your resume")
+    else:
+        await update.message.reply_html(
+            rf"Welcome to the Resume Tailoring Tool. go to /help to find out what you can do")
+
+
+async def handle_profile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text(
+        f"Welcome! Let's start editing your profile {update.effective_user.first_name}. What is your last name?"
+    )
+    print(update.message.from_user)
+    return LASTNAME
+
+
+async def last_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['last_name'] = update.message.text
+    await update.message.reply_text("What is your city?")
+    return CITY
+
+
+async def city(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['city'] = update.message.text
+    await update.message.reply_text("In which state do you live?")
+    return STATE
+
+
+async def state(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['state'] = update.message.text
+    await update.message.reply_text("In which country do you live?")
+    return COUNTRY
+
+
+async def country(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['country'] = update.message.text
+    await update.message.reply_text("Email")
+    return EMAIL
+
+
+async def email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['email'] = update.message.text
+    await update.message.reply_text("Phone number")
+    return PHONE
+
+
+async def phone(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data['phone'] = update.message.text
+    await update.message.reply_text("Linkedin Profile")
+    return LINKEDIN
+
+
+async def linkedin(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user_data = context.user_data
+    user_data['linkedin'] = update.message.text
+    await database.insert_data_user(connection, user_data['last_name'], user_data['city'], user_data['state'],
+                                    user_data['country'], user_data['email'],
+                                    user_data['phone'], user_data['linkedin'], update.effective_user.id)
+    await update.message.reply_text("Thank you for sharing your information! Now one more step. Please, add your "
+                                    "resume in a word document so I can store your information (Make sure the titles "
+                                    "for each section are named EDUCATION, EXPERIENCE, and SKILLS")
+    return RESUME
+
+
+async def resume(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    # context.user_data['phone'] = update.message.text
+    document_id = update.message.document
+    document = await context.bot.get_file(document_id)
+    print(f'Document: {document}')
+    print(f'Document ID: {document_id}')
+    file_path = Path(str(r"resumes/resume.docx"))
+    await document.download_to_drive(custom_path=file_path)
+    await read_doc(update, context, file_path)
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text('Bye! I hope we can talk again some day.')
+    return ConversationHandler.END
+
+
+async def read_doc(update: Update, context: ContextTypes.DEFAULT_TYPE, file_path) -> None:
+    doc = Document(file_path)
+    full_text = []
+    for word in doc.paragraphs:
+        full_text.append(word.text)
+    document_text = ' '.join(full_text)
+    print(document_text)
+    await update.message.reply_text("Thank you")
 
 
 async def tailor_resume(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -56,22 +145,22 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 
 # HANDLERS
-def echo_text(update, context):
-    text = update.message.text
-    username = ''
-    try:
-        username = update.message.chat.first_name
-    except:
-        pass
-    return 'What do you mean by "' + text + '"? ' + f"Please, check the /help command for hints of what to do next"
+# def echo_text(update, context):
+#     text = update.message.text
+#     username = ''
+#     try:
+#         username = update.message.chat.first_name
+#     except:
+#         pass
+#     return 'What do you mean by "' + text + '"? ' + f"Please, check the /help command for hints of what to do next"
 
 
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
+# async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     print(f'Update {update} caused error {context.error}')
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(echo_text(update, context))
+# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(echo_text(update, context))
 
 
 # MAIN
@@ -85,14 +174,31 @@ def main() -> None:
     app.add_handler(CommandHandler('tailorresume', tailor_resume))
     app.add_handler(CommandHandler('createcoverletter', create_cover_letter))
     app.add_handler(CommandHandler('jobnotifications', job_notifications))
-    # app.add_handler(CallbackQueryHandler(handle_response_query_user))
+    app.add_handler(CommandHandler('read_doc', read_doc))
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('profile', handle_profile)],
+        states={
+            LASTNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, last_name)],
+            CITY: [MessageHandler(filters.TEXT & ~filters.COMMAND, city)],
+            STATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, state)],
+            COUNTRY: [MessageHandler(filters.TEXT & ~filters.COMMAND, country)],
+            EMAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, email)],
+            PHONE: [MessageHandler(filters.TEXT & ~filters.COMMAND, phone)],
+            LINKEDIN: [MessageHandler(filters.TEXT & ~filters.COMMAND, linkedin)],
+            RESUME: [MessageHandler(filters.ATTACHMENT & ~filters.COMMAND, resume)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    app.add_handler(conv_handler)
 
     # Messages
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    # app.add_handler(MessageHandler(filters.PHOTO, check_origin_photo))
+    # app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    # app.add_handler(MessageHandler(filters.TEXT, conv_handler))
 
     # Errors
-    app.add_error_handler(error)
+    # app.add_error_handler(error)
 
     print('Polling...')
     app.run_polling(allowed_updates=Update.ALL_TYPES)
